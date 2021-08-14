de9dfb833c5802c49efcbfd977dabeac67cd0e5e
igormpmartins <73676465+igormpmartins@users.noreply.github.com>
2021-08-14 11:27:33 -0300

    simples teste

--- pages/api/getUser.js
+++ pages/api/getUser.js
@@ -1,7 +1,7 @@
 import getUser from '../../utils/getUser'
 
 const apiGetUser = async(req, res) => {
-    const data = await getUser('igormpmartins')
+    const data = await getUser('devpleno')
     res.send(data)
 }
 
